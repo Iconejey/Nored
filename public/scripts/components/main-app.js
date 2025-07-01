489998e2b97cb723a2b719e52875190e3c336fff
Nicolas Gouwy <43481095+Iconejey@users.noreply.github.com>
2025-07-01 21:25:04 +0000

    UI structure and animated logo

--- public/scripts/components/main-app.js
+++ public/scripts/components/main-app.js
@@ -1,3 +1,4 @@
+// This is a period cycle tracker application.
 class MainApp extends CustomElement {
 	constructor() {
 		super();
@@ -6,25 +7,43 @@ class MainApp extends CustomElement {
 		authFromURL();
 
 		// Wait for DOM to be ready
-		this.whenReady(() => {
+		this.whenReady(async () => {
 			// Log every storage change
 			STORAGE.onChange(console.log);
 
+			// Get current month and year
+			const date = new Date();
+			const current_month = date.getMonth();
+			const current_year = date.getFullYear();
+
+			// Get next month
+			const next_month = (current_month + 1) % 12;
+			const next_year = next_month === 0 ? current_year + 1 : current_year;
+
 			// Set the inner HTML
 			this.innerHTML = html`
-				<account-panel></account-panel>
-				<form>
-					<input type="text" name="content" placeholder="Some text" required />
-					<button type="submit">Write to storage</button>
-				</form>
+				<header>
+					<span class="icon">short_text</span>
+					<div class="logo hidden">
+						<img class="logo-drop" src="/drop.svg" />
+						<img class="logo-drop" src="/drop.svg" />
+						<img class="logo-drop" src="/drop.svg" />
+					</div>
+					<span class="icon hidden">close</span>
+				</header>
+
+				<div class="calendar">
+					<calendar-month year="${current_year}" month="${current_month}" />
+					<calendar-month year="${next_year}" month="${next_month}" />
+				</div>
+
+				<footer>Cliquez sur un jour où vous avez eu vos règles.</footer>
 			`;
 
-			// Handle form submit
-			this.$('form').onsubmit = async e => {
-				e.preventDefault();
-				const value = this.$('input').value;
-				await STORAGE.write('test.txt', value);
-			};
+			await delay(500);
+			this.$('.logo').classList.remove('hidden');
+			await delay(1200);
+			this.$('.logo').classList.add('bloom');
 		});
 	}
 }
